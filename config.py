01dc70c9fd4125c9826668d674baf000628891ed
notasnek <34934181+notasnek@users.noreply.github.com>
2025-08-01 11:19:39 +0300

    500k upd

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ DISCORD_WS_URL = "wss://gateway.discord.gg/?v=10&encoding-json" # discord ws
 
 CHILLI_HUB_CHANNELS_ID = {
     "under_500k": ["1394958052536619059", "1400301860178628739"], # under 500k channels
-    "500k_1m": ["1394958062166474823", "1400302142702751864"], # 500k - 1M channels
+    "500k_1m": ["1394958062166474823"], # 500k - 1M channels
     "1m-10m": ["1394958060828627064", "1400302392431349770"] # 1m - 10m channels
 }
 
