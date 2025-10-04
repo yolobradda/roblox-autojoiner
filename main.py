749ce816136681546613560b7b09e28f201503e9
yolobradda <145789234+yolobradda@users.noreply.github.com>
2025-10-04 13:53:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from src.roblox import roblox_main
 
 
 if __name__ == "__main__":
-    print("Roblox AutoJoiner for Chilli's Notify by notasnek")
+    print("Roblox AutoJoiner for Chilli's Notify by jvrre")
     print("Github: https://github.com/notasnek/roblox-autojoiner")
     print("Join our discord: https://discord.gg/fQSP3VFks9")
 
@@ -23,17 +23,18 @@ if __name__ == "__main__":
     print()
 
     print("Version: 1.1.1")
-    print("Starting in 2 seconds...")
+    print("Starting in 5 seconds...")
     print()
 
     # убирая мое авторство вы нарушаете лицензию (читай LICENSE.md)
     # By removing my authorship, you are violating the license (read LICENSE.md)
     # notasnek
 
-    time.sleep(2)
+    time.sleep(5)
 
     threading.Thread(target=roblox_main, daemon=True).start()
     asyncio.run(listener())
+
 
 
 # https://github.com/notasnek/roblox-autojoiner
